63676f0a5e9b0d6052e8667592e7bffd052c58ae
aryarnair <aryarnair1995@gmail.com>
2019-06-03 16:17:19 +0530

    water

--- hungry.c
+++ hungry.c
@@ -2,11 +2,14 @@ include <stdio.h>
 int main
 {
 printf("are you hungry?");
-int hungry;
+int hungry, thirsty;
 if(hungry==yes)
 printf("I'm hungry");
 else
-printf("I'm not hungry");
+
+printf("are you thirsty");
+if (thirsty==yes)
+printf("drink water");
 
 return 0;
 
